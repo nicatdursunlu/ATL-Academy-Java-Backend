297e68ff1c4aa86ce717a5cf3f10ad5680cdafc6
NijatD <nicat.dursunlu@gmail.com>
2022-11-03 14:24:53 +0400

    lesson-30: Update lesson-30 lecture

---
+++ sprng-boot/lesson-30/src/main/java/com/atl/academy/lesson30/Lesson30Application.java
@@ -0,0 +1,13 @@
+package com.atl.academy.lesson30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson30Application.class, args);
+    }
+
+}
