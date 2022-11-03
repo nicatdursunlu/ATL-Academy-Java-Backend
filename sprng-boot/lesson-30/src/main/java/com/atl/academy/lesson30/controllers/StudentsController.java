501399e418f089ca732e0533285a99db2c4c2216
NijatD <nicat.dursunlu@gmail.com>
2022-11-03 14:41:17 +0400

    lesson-30: Update lesson-30 lecture

--- sprng-boot/lesson-30/src/main/java/com/atl/academy/lesson30/controllers/StudentsController.java
+++ sprng-boot/lesson-30/src/main/java/com/atl/academy/lesson30/controllers/StudentsController.java
@@ -1,7 +1,9 @@
 package com.atl.academy.lesson30.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +13,12 @@ public class StudentsController {
     @ResponseBody
     public String helloAtl() {
         return "Hello ATL Academy";
+    }
+
+    @GetMapping("/api/foos")
+    @ResponseBody
+    public String getFoos(@RequestParam String id) {
+        return "ID: " + id;
     }
 }
 
